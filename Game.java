6b388c193fe06d9df03660fcc36a0b8da1313cea
ozgurb2005-bit <ozgurb2005@gmail.com>
2026-05-04 21:45:06 -0400

    Updated game and game library

--- Game.java
+++ Game.java
@@ -1,62 +1,130 @@
-import java.util.HashSet;
-
 /**
- * Write a description of class Game here.
- *
- * @author (Rushane)
- * @version (a version number or a date)
+ * Represents a video game.
+ * 
+ * @author Rushane
  */
 public class Game
 {
-    // instance variables - replace the example below with your own
     private String gameName;
     private String developer;
     private double version;
     private PlatformType platform;
+    private GenreType genre;
+    private double rating;
     private boolean isPaused;
 
-    /**
-     * Constructor for objects of class Game
-     */
-    public Game()
+    public Game(String gameName, String developer, double version,
+                PlatformType platform, GenreType genre, double rating)
     {
-        
+        this.gameName = gameName;
+        this.developer = developer;
+        this.version = version;
+        this.platform = platform;
+        this.genre = genre;
+        this.rating = rating;
+        this.isPaused = false;
     }
 
-    /**
-     * 
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public void startGame()
     {
-        // initialise instance variables
         isPaused = false;
-        System.out.println(gameName + "started.");
-
+        System.out.println(gameName + " started.");
     }
-    
-    /**
-     * 
-     */
+
     public void pauseGame()
     {
         isPaused = true;
     }
-    
-    /**
-     * 
-     */public void  resumeGame()
+
+    public void resumeGame()
     {
         isPaused = false;
     }
-    
-    /**
-     */
+
     public void stopGame()
     {
-        System.out.println("Game stopped");
+        System.out.println("Game stopped.");
     }
-}   
 
+    public String getGameName() 
+    { 
+        return gameName; 
+    }
+    
+    public String getDeveloper() 
+    { 
+        return developer; 
+    }
+    
+    public double getVersion() 
+    { 
+        return version; 
+    }
+    
+    public PlatformType getPlatform() 
+    { 
+        return platform; 
+    }
+    
+    public GenreType getGenre() 
+    { 
+        return genre; 
+    }
+    
+    public double getRating() 
+    { 
+        return rating; 
+    }
+    
+    public boolean isPaused() 
+    { 
+        return isPaused; 
+    }
+
+    public void setGameName(String gameName) 
+    { 
+        this.gameName = gameName; 
+    }
+    
+    public void setDeveloper(String developer) 
+    { 
+        this.developer = developer; 
+    }
+    
+    public void setVersion(double version) 
+    { 
+        this.version = version; 
+    }
+    
+    public void setPlatform(PlatformType platform) 
+    { 
+        this.platform = platform; 
+    }
+    
+    public void setGenre(GenreType genre) 
+    { 
+        this.genre = genre; 
+    }
+    
+    public void setRating(double rating) 
+    { 
+        this.rating = rating; 
+    }
+
+    @Override 
+    public String toString()
+    {
+        return gameName + " by " + developer + " (" + platform + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Game other = (Game) obj;
+        return gameName.equals(other.gameName) &&
+               developer.equals(other.developer);
+    }
+}
--- GameLibrary.java
+++ GameLibrary.java
@@ -2,10 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Write a description of class GameLibrary here.
- *
- * @author (Rushane)
- * @version (a version number or a date)
+ * Represents a collection of games.
+ * 
+ * @author Rushane
  */
 public class GameLibrary
 {
@@ -14,21 +13,117 @@ public class GameLibrary
     private AgeRating ageRating;
     private double gameRating;
 
-    /**
-     * Constructor for objects of class GameLibrary
-     */
     public GameLibrary()
     {
-        // initialise instance variables
-        games = new ArrayList<Game>();
+        games = new ArrayList<>();
     }
 
-    /**
-     * A method to add a game
-     *
-     */
     public void addGame(Game game)
     {
-        games.add(game);           
+        games.add(game);
+    }
+
+    public void removeGame(Game game)
+    {
+        games.remove(game);
+    }
+
+    public int getGameCount()
+    {
+        return games.size();
+    }
+
+    public List<Game> findByGenre(GenreType genre)
+    {
+        List<Game> result = new ArrayList<>();
+        for (Game g : games)
+        {
+            if (g.getGenre() == genre)
+            {
+                result.add(g);
+            }
+        }
+        return result;
+    }
+
+    public List<Game> findByRating(double rating)
+    {
+        List<Game> result = new ArrayList<>();
+        for (Game g : games)
+        {
+            if (g.getRating() >= rating)
+            {
+                result.add(g);
+            }
+        }
+        return result;
+    }
+
+    public Game selectGame(int index)
+    {
+        if (index >= 0 && index < games.size())
+        {
+            return games.get(index);
+        }
+        return null;
+    }
+
+    public void playGame(Game game)
+    {
+        if (game != null)
+        {
+            game.startGame();
+        }
+    }
+
+    public List<Game> getGames() 
+    {
+        return games; 
+    }
+
+    public GenreType getGameGenre() 
+    {
+        return gameGenre; 
+    }
+    
+    public void setGameGenre(GenreType gameGenre) 
+    { 
+        this.gameGenre = gameGenre; 
+    }
+
+    public AgeRating getAgeRating() 
+    { 
+        return ageRating; 
+    }
+    
+    public void setAgeRating(AgeRating ageRating) 
+    { 
+        this.ageRating = ageRating; 
+    }
+
+    public double getGameRating() 
+    { 
+        return gameRating; 
+    }
+    
+    public void setGameRating(double gameRating) 
+    { 
+        this.gameRating = gameRating; 
+    }
+
+    @Override
+    public String toString()
+    {
+        return "GameLibrary with " + games.size() + " games.";
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        GameLibrary other = (GameLibrary) obj;
+        return games.equals(other.games);
     }
 }
