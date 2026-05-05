e54733ca092033ebed1eae72d4ce96ab15a7d68b
Shane2203 <rushanechryzl@gmail.com>
2026-05-04 22:29:21 -0400

    dwdw

--- Multiplayer.java
+++ Multiplayer.java
@@ -15,13 +15,39 @@ public class Multiplayer extends Game
     /**
      * Constructor for objects of class Multiplayer
      */
-    public Multiplayer()
+    public Multiplayer(String name,PlatformType platform,int maxPlayers,GameModeType mode)
     {
+        //name, developer, version, platform, genre, rating
+        super(name, "Unknown", 1.0, platform, GenreType.ACTION, 0.0);
         this.maxPlayers = maxPlayers;
-        this.playerCount = playerCount;
+        this.playerCount = 0; // Starts with no players
+        this.gameMode = mode;
         
     }
-    
+    public int getMaxPlayers()
+    {
+        return maxPlayers;
+    }
+    public void setMaxPlayers(int maxPlayers)
+    {
+        this.maxPlayers = maxPlayers;
+    }
+    public int getPlayerCount()
+    {
+        return playerCount;
+    }
+    public void setPlayerCount(int playerCount)
+    {
+        this.playerCount = playerCount;
+    }
+    public GameModeType getGameMode()
+    {
+        return gameMode;
+    }
+    public void setGameMode(GameModeType mode)
+    {
+        this.gameMode = mode;
+    }
     public void addPlayer()
     {
         if(!isLobbyFull()){
@@ -37,5 +63,19 @@ public class Multiplayer extends Game
     public void changeGameMode(GameModeType mode)
     {
         this.gameMode = mode;
+    }
+    @Override
+    public String toString()
+    {
+        return super.toString() + " | Mode: " + gameMode + " Players: " + playerCount + "/" + maxPlayers;  
+    }
+    @Override
+    public boolean equals(Object obj)
+    { 
+        if (this == obj) return true;
+        if(!(obj instanceof Multiplayer)) return false;
+        if(!super.equals(obj)) return false;
+        Multiplayer other = (Multiplayer) obj;
+        return this.maxPlayers == other.maxPlayers && this.gameMode == other.gameMode;
     }
 }
