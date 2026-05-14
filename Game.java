259f6ffe873244caf694d1e3c415aa77faf94731
Shane2203 <rushanechryzl@gmail.com>
2026-05-14 11:19:19 -0400

    edit

--- Game.java
+++ Game.java
@@ -36,7 +36,7 @@ public class Game
         this.genre = genre;
         this.rating = rating;
         this.isPaused = false;
-        new Game(name, developer, version, platform, genre, rating); 
+ 
     }
 
     public void startGame()
